a9a09cb4045d7bcc360d0c324271899d3c33e082
Akash604-crypto <71256281+Akash604-crypto@users.noreply.github.com>
2025-12-28 01:55:28 +0530

    Update config.py

--- telegram-video-pipeline/user_bridge/config.py
+++ telegram-video-pipeline/user_bridge/config.py
@@ -1,9 +1,8 @@
 API_ID = 35343847
-API_HASH = "f85d7a640630d63430fc34ca424a518f"
+API_HASH = -1002349383274
 
 CHANNEL1 = "https://t.me/+5ryEASHiFtE2MjRi"
 BOT1_USERNAME = "@Terabof5bot"
 YOUR_BOT_USERNAME = "@Forwarjdjd_bot"
 
 SAFE_DELAY = 3  # seconds
-REPLY_TIMEOUT = 60
